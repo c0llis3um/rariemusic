5baa031c4c66a8862297e43524fbc628e9456bfd
Joel Alcantara <joelalcan@gmail.com>
2011-12-30 23:06:26 -0500

    New changes

--- index.php
+++ index.php
@@ -1,1 +1,135 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><title>Rarie Music Podcast Techno House </title><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><meta http-equiv="Content-Language" content="us" /><meta http-equiv="Content-Language" content="en" /><meta name="keywords" content="music, electronica, house,"/><meta name='description' content='recording'/><meta name='author' content='esho'/><meta name='copyright' content='Copyright (c) 2012 - rarie music'/><meta name='creator' content='rarie music'/><meta name='distribution' content='Global'/><meta name='rating' content='General'/><meta name='robots' content='All'/><meta name='revisit-after' content='2 Days'/><link rel="shortcut icon" href="favicon.ico" /><link rel="stylesheet" type="text/css" href="hoja1.css" /><script type="text/javascript">  var _gaq = _gaq || [];  _gaq.push(['_setAccount', 'UA-4299780-13']);  _gaq.push(['_trackPageview']);  (function() {    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);  })();</script></head><body><div id="fb-root"></div><script>(function(d, s, id) {  var js, fjs = d.getElementsByTagName(s)[0];  if (d.getElementById(id)) {return;}  js = d.createElement(s); js.id = id;  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=231373313598772";  fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));</script><div id="contenedor"><div id="logo"><a  href="index.html"><img src="images/rarie-logo.png" alt="logo" width="" height="" border="0px"/></a></div><div id="navbar"><div><ul id="navlist"><li id="active" ><a href="#" id="current">music</a><ul id="subnavlist"><li><a href="#">Chris Fortier</a></li><li id="subactive"><a href="#" id="subcurrent">Chloe Harris</a></li><li><a href="jordiriera.html">Jordi Riera</a></li><li><a href="#">Mike Hiratzka</a></li></ul></li><li><a href="#">podcast</a></li><li><a href="#">tde-blog</a></li><li><a href="#">contact</a></li></ul></div><input type="text" name="search" size="20" class="s1"></div><div id="header"><table border="0" class="t1"> <tr> <td><img src="images/jdmh-re1.png" /></td> <td><h1 class="hh">Jordi Riera & Mike Hiratzka<br /> “Right Place, Right Time (Original Mix)”<br /> </h1><object height="81" width="100%" class="hhh"> <param name="movie" value="http://player.soundcloud.com/player.swf?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F6394841%3Fsecret_token%3Ds-hs3an&amp;show_comments=true&amp;auto_play=false&amp;color=646464"></param> <param name="allowscriptaccess" value="always"></param> <embed allowscriptaccess="always" height="81" src="http://player.soundcloud.com/player.swf?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F6394841%3Fsecret_token%3Ds-hs3an&amp;show_comments=true&amp;auto_play=false&amp;color=646464" type="application/x-shockwave-flash" width="100%"></embed> </object> <a href="http://soundcloud.com/rariemusic" class="hhh">by Rarie Music</a></span></td> </table></div><div id="bloque1"><h1>Testing</h1><p>Nulla neque enim, commodo non faucibus vitae, ornare vitae ante. Quisque ut lectus id libero dapibus dapibus at ut tellus. </p></div><div id="bloque2"><h1>Testing</h1><p>Nulla neque enim, commodo non faucibus vitae, ornare vitae ante. Quisque ut lectus id libero dapibus dapibus at ut tellus. </p></div><div id="pie">Rarie Music All Rights Reserved 2012 &copy; Visit <a href="http://www.soundcloud.com" target="_blank">SoundCloud</a></div><div id="subpie"><div class="fb-like" data-href="https://www.facebook.com/home.php#!/rariemusic" data-send="true" data-width="450" data-show-faces="true"></div></div></div></div></body></html>
+<?php
+/*
+*************************************************************************
+	MODx Content Management System and PHP Application Framework 
+	Managed and maintained by Raymond Irving, Ryan Thrash and the
+	MODx community
+*************************************************************************
+	MODx is an opensource PHP/MySQL content management system and content
+	management framework that is flexible, adaptable, supports XHTML/CSS
+	layouts, and works with most web browsers, including Safari.
+
+	MODx is distributed under the GNU General Public License	
+*************************************************************************
+
+	MODx CMS and Application Framework ("MODx")
+	Copyright 2005 and forever thereafter by Raymond Irving & Ryan Thrash.
+	All rights reserved.
+
+	This file and all related or dependant files distributed with this filie
+	are considered as a whole to make up MODx.
+
+	MODx is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	MODx is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with MODx (located in "/assets/docs/"); if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+
+	For more information on MODx please visit http://modxcms.com/
+	
+**************************************************************************
+    Originally based on Etomite by Alex Butter
+**************************************************************************
+*/	
+
+/**
+ * Initialize Document Parsing
+ * -----------------------------
+ */
+
+// get start time
+$mtime = microtime(); $mtime = explode(" ",$mtime); $mtime = $mtime[1] + $mtime[0]; $tstart = $mtime;
+
+// harden it
+require_once(dirname(__FILE__).'/manager/includes/protect.inc.php');
+
+// set some settings, and address some IE issues
+@ini_set('url_rewriter.tags', '');
+@ini_set('session.use_trans_sid', 0);
+@ini_set('session.use_only_cookies',1);
+session_cache_limiter('');
+header('P3P: CP="NOI NID ADMa OUR IND UNI COM NAV"'); // header for weird cookie stuff. Blame IE.
+header('Cache-Control: private, must-revalidate');
+ob_start();
+error_reporting(E_ALL & ~E_NOTICE);
+
+/**
+ *	Filename: index.php
+ *	Function: This file loads and executes the parser. *
+ */
+
+define("IN_ETOMITE_PARSER", "true"); // provides compatibility with etomite 0.6 and maybe later versions
+define("IN_PARSER_MODE", "true");
+define("IN_MANAGER_MODE", "false");
+
+if (!defined('MODX_API_MODE')) {
+    define('MODX_API_MODE', false);
+}
+
+// initialize the variables prior to grabbing the config file
+$database_type = '';
+$database_server = '';
+$database_user = '';
+$database_password = '';
+$dbase = '';
+$table_prefix = '';
+$base_url = '';
+$base_path = '';
+
+// get the required includes
+if($database_user=="") {
+	$rt = @include_once(dirname(__FILE__).'/manager/includes/config.inc.php');
+	// Be sure config.inc.php is there and that it contains some important values
+	if(!$rt || !$database_type || !$database_server || !$database_user || !$dbase) {
+	echo "
+<style type=\"text/css\">
+*{margin:0;padding:0}
+body{margin:50px;background:#eee;}
+.install{padding:10px;border:5px solid #f22;background:#f99;margin:0 auto;font:120%/1em serif;text-align:center;}
+p{ margin:20px 0; }
+a{font-size:200%;color:#f22;text-decoration:underline;margin-top: 30px;padding: 5px;}
+</style>
+<div class=\"install\">
+<p>MODx is not currently installed or the configuration file cannot be found.</p>
+<p>Do you want to <a href=\"install/index.php\">install now</a>?</p>
+</div>";
+		exit;
+	}
+}
+
+// start session 
+startCMSSession();
+
+// initiate a new document parser
+include_once(MODX_MANAGER_PATH.'/includes/document.parser.class.inc.php');
+$modx = new DocumentParser;
+$etomite = &$modx; // for backward compatibility
+
+// set some parser options
+$modx->minParserPasses = 1; // min number of parser recursive loops or passes
+$modx->maxParserPasses = 10; // max number of parser recursive loops or passes
+$modx->dumpSQL = false;
+$modx->dumpSnippets = false; // feed the parser the execution start time
+$modx->tstart = $tstart;
+
+// Debugging mode:
+$modx->stopOnNotice = false;
+
+// Don't show PHP errors to the public
+if(!isset($_SESSION['mgrValidated']) || !$_SESSION['mgrValidated']) {
+    @ini_set("display_errors","0");
+}
+
+// execute the parser if index.php was not included
+if (!MODX_API_MODE) {
+    $modx->executeParser();
+}
+?>
